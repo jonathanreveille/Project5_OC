2d2e135a334f6ab18ec7e31a0bcf94c67b951be0
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-12-12 11:30:02 +0100

    clearing out useless files

--- bdd/database.py
+++ bdd/database.py
@@ -4,15 +4,14 @@
 from peewee import MySQLDatabase
 from settings.constants import NAME_DB, USER, PASSWORD, HOST, PORT, CHARSET
 
+
 db = MySQLDatabase(NAME_DB, user=USER, password=PASSWORD,
-host=HOST, port=PORT, charset=CHARSET)
+                   host=HOST, port=PORT, charset=CHARSET)
 
 
 def main():
     pass
 
+
 if __name__ == "__main__":
     pass
-
-
-
