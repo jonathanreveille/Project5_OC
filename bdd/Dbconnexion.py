62817493f885e20a3e8704d56aaaa750754cd701
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-12-12 17:12:50 +0100

    new name for file

---
+++ bdd/Dbconnexion.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+from peewee import MySQLDatabase
+from settings.constants import NAME_DB, USER, PASSWORD, HOST, PORT, CHARSET
+
+
+db = MySQLDatabase(NAME_DB, user=USER, password=PASSWORD,
+                   host=HOST, port=PORT, charset=CHARSET)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
