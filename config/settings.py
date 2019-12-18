a98773dbb7b108bece4e4a8bef3bf398c946c50e
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-12-18 17:04:42 +0100

    settings

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+""" This module is created to stock all
+constants for this application """
+
+# Database connexion for application - MySQL
+NAME_DB = 'p5_db'
+USER = 'student'
+PASSWORD = 'student'
+HOST = '127.0.0.1'
+PORT = 3306
+CHARSET = 'utf8mb4'
+
+
+# Keywords
+#KEYWORDS = ["product_name_fr", "main_category_fr",
+            #"nutrition_grade_fr", "id", "br  ands", "stores"]
