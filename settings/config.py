e9135aa9ddc8e7a8d26535a1535338581173cb5c
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-01-08 12:17:10 +0100

    config file with list of categories, not used so far

---
+++ settings/config.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+""" This module is where all constants are stocked """
+
+
+CATEGORY_LIST = ["biscuits", "pizza", "jus de fruit", "confiture", "pâte salée à tartiner"]
+
+
+
