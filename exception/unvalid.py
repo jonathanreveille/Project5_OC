bdba6c62198c88cdea294bb3cdafa274eb0c3137
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-01-08 12:17:32 +0100

    ValueError Exception module, raise error for user

---
+++ exception/unvalid.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+""" This module was created to allow pop up when the input
+is unvalid """
+
+class UnvalidInput(Exception):
+    """ A class when the user inserts an unvalid input """
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
