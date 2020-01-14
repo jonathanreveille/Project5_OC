b78c7209b8a8d8cc7215ffedce215604f6ff671e
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-01-14 14:17:11 +0100

    testing zone

---
+++ menu/test.py
@@ -0,0 +1,13 @@
+categories = ["cat1", "cat2", "cat3"]
+
+x = "___".join(categories)
+print(x)
+# result >>> cat1___cat2___cat3
+
+y = "/n".join(categories)
+print(y)
+# result >>> cat1/ncat2/ncat3
+
+
+
+
