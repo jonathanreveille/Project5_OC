c5b84e1fd9fb4d7223a69d837270f28cdec38e65
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2020-01-28 16:07:42 +0100

    added all constant to connect to database

--- settings/config.py
+++ settings/config.py
@@ -3,4 +3,13 @@
 
 """This module is where all constants are stocked."""
 
-CATEGORY_LIST = ["biscuits", "pizza", "jus de fruit", "chips", "confiture", "fromage", "soda"]
+# Database connexion for application - MySQL
+NAME_DB = 'p5_db'
+USER = 'student'
+PASSWORD = 'student'
+HOST = '127.0.0.1'
+PORT = 3306
+CHARSET = 'utf8mb4'
+
+# For OpenFoodFact API
+CATEGORY_LIST = ["biscuits", "pizza", "jus de fruit", "chips", "confiture", "fromage"]
